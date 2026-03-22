fb453cda7ef912dd0c94d49b30f2a6b513d9ddc3
chrisjcoates <chriscoates9@gmail.com>
2026-03-22 12:42:22 +0000

    Created BattleService class to simulate hero and enemy attacks, and the mainn battle.

---
+++ HeroForge/Services/BattleService.cs
@@ -0,0 +1,59 @@
+using HeroForge.Models;
+using HeroForge.Helpers;
+
+namespace HeroForge.Services
+{
+    public static class BattleService
+    {
+        public static void HeroAttacks(Hero hero, Enemy enemy)
+        {
+            var damage = enemy.Defence - hero.Defence;
+            if (damage < 1) damage = 1;
+            
+            enemy.CurrentHp -= damage;
+            
+            Console.WriteLine($"{hero.Name} attacks {enemy.Name} dealing {damage} damage.\n");
+            DisplayHelper.DisplayStats(hero);
+        }
+
+        public static void EnemyAttacks(Hero hero, Enemy enemy)
+        {
+            var damage = hero.Defence - enemy.AttackPower;
+            if (damage < 1) damage = 1;
+            
+            hero.CurrentHp -= damage;
+            
+            Console.WriteLine($"{enemy.Name} attacks {hero.Name} dealing {damage} damage.\n");
+            DisplayHelper.DisplayStats(enemy);
+        }
+
+        public static void StartBattle(Hero hero, Enemy enemy)
+        {
+            Console.WriteLine($"=== BATTLE COMMENCING! ===");
+            Console.WriteLine($"{hero.Name} vs {enemy.Name}");
+
+            do
+            {
+                HeroAttacks(hero, enemy);
+
+                if (enemy.CurrentHp <= 0) break;
+                
+                EnemyAttacks(hero, enemy);
+                
+                if (hero.CurrentHp <= 0) break;
+
+            } while (hero.CurrentHp > 0 || enemy.CurrentHp > 0);
+
+            if (hero.CurrentHp > 0)
+            {
+                Console.WriteLine("=== The hero is victorious ===");
+                Console.WriteLine($"{hero.Name} defeated {enemy.Name}! Another tale for the hall of heroes!");
+            }
+            else
+            {
+                Console.WriteLine("=== The hero is defeated! ===");
+                Console.WriteLine($"{hero.Name} has been defeated by {enemy.Name}, shame...");
+            }
+        }
+    }
+}
