5509d5cba397dfe4cc440674c6ab47807cc0f77b
chrisjcoates <chriscoates9@gmail.com>
2026-03-22 13:48:29 +0000

    Added contructor for Hero.

--- HeroForge/Models/Hero.cs
+++ HeroForge/Models/Hero.cs
@@ -6,7 +6,20 @@ namespace HeroForge.Models
         public string? CharacterClass { get; set; }
         public int MaxHp { get; set; }
         public int CurrentHp { get; set; }
-        public int AttackPower  { get; set; }
+        public int AttackPower { get; set; }
         public int Defence { get; set; }
+        
+        public Hero(string name, string characterClass)
+        {
+            var random = new Random();
+
+            Name = name;
+            CharacterClass = characterClass;
+            MaxHp = random.Next(10, 80);
+            CurrentHp = MaxHp;
+            AttackPower = random.Next(10, 20);
+            Defence = random.Next(10, 20);
+        }
     }
 }
+
