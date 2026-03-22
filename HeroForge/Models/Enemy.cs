e1185904c25270266881b3cd6bb096c75c434386
chrisjcoates <chriscoates9@gmail.com>
2026-03-22 12:40:26 +0000

    Created ememy class

---
+++ HeroForge/Models/Enemy.cs
@@ -0,0 +1,11 @@
+namespace HeroForge.Models
+{
+    public class Enemy
+    {
+        public string? Name { get; set; }
+        public int MaxHp { get; set; }
+        public int CurrentHp { get; set; }
+        public int AttackPower { get; set; }
+        public int Defence { get; set; }
+    }
+}
