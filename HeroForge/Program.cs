c223df5ac602a180d4164118fc40a18e1da72697
chrisjcoates <chriscoates9@gmail.com>
2026-03-22 13:28:11 +0000

    Added input validation. Console clears and /n to keep the console clean and readable.

--- HeroForge/Program.cs
+++ HeroForge/Program.cs
@@ -5,8 +5,8 @@ using HeroForge.Services;
 
 var appRunning = true;
 
-Console.WriteLine("=== Welcome to HeroForge! ===");
-Console.WriteLine("Create a hero and test their strength against a random enemy!");
+Console.WriteLine("=== Welcome to HeroForge! ===\n");
+Console.WriteLine("Create a hero and test their strength against a random enemy!\n");
 do
 {   
     Console.Write("Enter a name for your character: ");
@@ -14,47 +14,73 @@ do
 
     string[] classes = ["Warrior", "Mage", "Rouge"];
     
-    Console.WriteLine("Classes:");
+    Console.WriteLine("\nClasses:\n");
     for (var i = 0; i < classes.Length; i++)
     {
         Console.WriteLine($"{i + 1}. {classes[i]}");
     }
-    Console.Write($"Choose a class from the below list (1-{classes.Length}: )");
-    var playerClass = Convert.ToInt32(Console.ReadLine());
+    bool validInt;
+    int classChoice;
+    do
+    {
+        Console.Write($"\nChoose a class from the below list (1-{classes.Length}): ");
+        var playerClassChoice = Console.ReadLine();
+        if (int.TryParse(playerClassChoice, out classChoice) &&
+            classChoice >= 1 &&
+            classChoice <= classes.Length)
+        {
+            validInt = true;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input, try again.");
+            validInt = false;
+        }
+        
+    } while (!validInt);
     
     var random = new Random();
     var newHero = new Hero
     {
-        Name = playerName, CharacterClass = classes[playerClass], MaxHp = random.Next(10, 100), AttackPower = random.Next(10, 20), Defence = random.Next(10, 20),
+        Name = playerName, CharacterClass = classes[classChoice -1], MaxHp = random.Next(10, 80), AttackPower = random.Next(10, 20), Defence = random.Next(10, 20),
     };
     newHero.CurrentHp = newHero.MaxHp;
-    
-    DisplayHelper.DisplayStats(newHero);
 
+    Console.Clear();
+    
+    Console.WriteLine($"{newHero.Name} the {newHero.CharacterClass} has been created.");
+    DisplayHelper.DisplayStats(newHero);
+    
     var newEnemy = EnemyFactory.GenerateEnemy();
+    Console.WriteLine($"A {newEnemy.Name} has been created.");
+    DisplayHelper.DisplayStats(newEnemy);
+
+    Console.WriteLine("Click any key to continue...");
+    Console.ReadKey();
+    Console.Clear();
     
     BattleService.StartBattle(newHero, newEnemy);
-
-    Console.Write("Would you like to play again? y/n: ");
-    var validResponse = false;
+    
+    bool validResponse;
     do
     {
+        Console.Write("Would you like to play again? y/n: ");
         var playAgain = Console.ReadLine();
-        switch (playAgain)
+        switch (playAgain?.ToLower())
         {
             case "y":
-                validResponse = true;
+                validResponse = false;
                 break;
             case "n":
-                validResponse = true;
+                Console.WriteLine("\nExiting HeroForge...");
+                validResponse = false;
                 appRunning = false;
                 break;
             default:
-                validResponse = false;
+                validResponse = true;
                 Console.WriteLine($"{playAgain} is not a valid choice.");
                 break;
         }
-    } while (!validResponse);
-
-
+    } while (validResponse);
+    
 } while (appRunning);
